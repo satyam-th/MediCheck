48f5ef0731d5f80327c9b2ad6bd5f97a1f02da84
Samiksha063 <samiksha3stha@gmail.com>
2026-06-02 16:05:18 +0545

    setup routing and basic layout

---
+++ frontend/src/pages/user/Home.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../../components/layout/NavBar";
+import Footer from '../../components/layout/Footer'
+export default function Home(){
+    return(
+        <>
+        <NavBar/>
+        <h1>Home Page....</h1>
+        <Footer/>
+        </>
+    );
+}
