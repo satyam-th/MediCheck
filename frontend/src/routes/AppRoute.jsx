a93d4a1cb35dc14f3ec1f0216deec2f245726a02
Samiksha063 <samiksha3stha@gmail.com>
2026-06-22 23:29:55 +0545

    add separate customer and pharmacy registration and update the routes

--- frontend/src/routes/AppRoute.jsx
+++ frontend/src/routes/AppRoute.jsx
@@ -2,8 +2,10 @@ import {Routes, Route} from 'react-router-dom';
 
 import Home from '../pages/user/Home/Home';
 import Login from '../pages/auth/login/Login';
-import Register from '../pages/auth/register/Register';
-// import Pharmacy from '../pages/pharmacy/Dashboard';
+import RegisterCustomer from '../pages/auth/register/RegisterCustomer';
+import RegisterPharmacy from '../pages/auth/register/RegisterPharmacy';
+
+import PharmacyDashboard from '../pages/pharmacy/Dashboard/Dashboard'
 // import Admin from '../pages/admin/Dashboard'
 
 export default function AppRoute(){
@@ -11,9 +13,11 @@ export default function AppRoute(){
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-            {/* <Route path='/pharmacy' element={<Pharmacy/>}/>
-            <Route path='/admin' element={<Admin/>}/> */}
+            <Route path='/register/customer' element={<RegisterCustomer/>}/>
+            <Route path='/register/pharmacy' element={<RegisterPharmacy/>}/>
+
+            <Route path='/pharmacy/dashboard' element={<PharmacyDashboard/>}/>
+            {/* <Route path='/admin' element={<Admin/>}/> */}
         </Routes>
     );
 }
